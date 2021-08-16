bd0fb408b1a66bf01b07cc68cf5f953fe045440c
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-08-16 22:36:02 +0800

    - Menggunakan Constructor Pada Python

---
+++ 2.Constuctor/Main.py
@@ -0,0 +1,12 @@
+class Hero:
+
+    # Constructor Pada Python
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+
+hero1 = Hero("Eudora", 100)
+
+print(hero1.name)
+print(hero1.health)
